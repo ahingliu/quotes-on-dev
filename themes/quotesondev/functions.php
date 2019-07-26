60347ab3b35fd5e5f99f9fcf98fc6d09ae72f5b8
Alex Hing Liu <hingliu6@gmail.com>
2019-07-25 20:53:37 -0700

    hierarchy structuring done

--- themes/quotesondev/functions.php
+++ themes/quotesondev/functions.php
@@ -82,3 +82,17 @@ require get_template_directory() . '/inc/metaboxes.php';
  * Custom WP API modifications.
  */
 require get_template_directory() . '/inc/api.php';
+
+//testing single page
+
+function red_scripts() {
+	$script_url = get_template_directory_uri() . '../scripts.js';
+	wp_enqueue_script( 'jquery' );
+	wp_enqueue_script( 'red_comments', $script_url, array( 'jquery' ), false, true );
+   wp_localize_script( 'red_comments', 'red_vars', array(
+	   'ajax_url' => admin_url( 'admin-ajax.php' ),
+	   'comment_nonce' => wp_create_nonce( 'red_comment_status' ),
+	   'post_id' => get_the_ID()
+   ) );
+  }
+  add_action( 'wp_enqueue_scripts', 'red_scripts' );
