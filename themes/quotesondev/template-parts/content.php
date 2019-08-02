38bd6698333effc8ae8d6214c7d5af802fa3497a
Alex Hing Liu <hingliu6@gmail.com>
2019-08-01 23:31:42 -0700

    about template fix

--- themes/quotesondev/template-parts/content.php
+++ themes/quotesondev/template-parts/content.php
@@ -1,35 +1,18 @@
 <?php
 /**
- * The main template file.
+ * Template part for displaying posts.
  *
  * @package QOD_Starter_Theme
  */
 
-get_header(); ?>
+?>
 
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+	<header class="entry-header">
+		<?php the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' ); ?>
+	</header><!-- .entry-header -->
 
-			<?php $args = array(
-				'posts_per_page' => 1,
-				'orderby' => 'rand'
-			);
-			?>
-			<?php /* Start the Loop */ ?>
-			<?php query_posts($args); if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-
-				<?php get_template_part( 'template-parts/content' ); ?>
-				<button type="button" class="anotherone" id="another">Show Me Another</button>
-			
-			<?php endwhile; ?>
-			<?php else : ?>
-
-			<?php get_template_part( 'template-parts/content', 'none' ); ?>
-
-			<?php endif; wp_reset_query() ?>
-
-		</main><!-- #main -->
-	</div><!-- #primary -->
-
-<?php get_footer(); ?>
-
+	<div class="entry-content">
+		<?php the_excerpt(); ?>
+	</div><!-- .entry-content -->
+</article><!-- #post-## -->
